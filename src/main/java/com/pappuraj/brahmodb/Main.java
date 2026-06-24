d751890459514ff258b867d4b8c2cf0d2136378b
PAPPURAJ <pappuraj.duet@gmail.com>
2026-06-24 11:08:03 +0600

    Initial BrahmoDB project setup

---
+++ src/main/java/com/pappuraj/brahmodb/Main.java
@@ -0,0 +1,8 @@
+package com.pappuraj.brahmodb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BrahmoDB started successfully.");
+        System.out.println("Oracle-inspired relational database engine from scratch in Java.");
+    }
+}
